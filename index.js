976e3e7da016ca6f00a767fb1daaf4d14a60c16a
Juan Leon <Juan Leon>
2016-11-03 14:31:43 -0700

    updating apps post graduation

--- index.js
+++ index.js
@@ -2,8 +2,50 @@
 // This function should grab the values entered by the user from the input element and the select element.
 // The function should then calculate the weight of the user on the selected planet, and show the weight to the user in the p element you added to your HTML.
 
+/*
+$("#btnCalculateWeight").click(function(){
+var userWeight = S("#userWeight").val();
+var planetText = $('#planetWeight option:selected').text();
+var planetMultiplier = S('planetValue option:selected').val();
+var weightOnPlanet = userWeight * planetMultiplier;
+
+$("#output").text('If you were on') + ' + ' planetName 'you would weight' + weightOnPlanet 'lbs'
+});
 
 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function calculateweight() {
+  var userWeight = document.getElementById('userWeight').value;
+  var select = document.getElementById('planetWeight');
+  var options select.options;
+  var selectedOption = option [select.selectedIndex];
+  var planetName = selectedOption.innerHTML;
+  var planetMultiplier = selectedOption.value;
+  var weightOnPlanet= userWeight * planetWeight;
+
+
+}*/
 
 
      function weightCalculate(){  
